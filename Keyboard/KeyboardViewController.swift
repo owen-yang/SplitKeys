f3fcb74cedf227c49a5f2401a282630455064e75
owen-yang <yoyoyango123@gmail.com>
2016-10-03 20:54:42 -0400

    Added basic dual key mode, inputting 'A' and 'B' to text field

--- Keyboard/KeyboardViewController.swift
+++ Keyboard/KeyboardViewController.swift
@@ -9,36 +9,110 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
-
-    @IBOutlet var nextKeyboardButton: UIButton!
     
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
+    var doubleView: UIView
+    var divider: UIView
+    var leftButton: UIView
+    var rightButton: UIView
+    
+    var leftTapGestureRecognizer: UITapGestureRecognizer!
+    var rightTapGestureRecognizer: UITapGestureRecognizer!
+    
+    var nextKeyboardButton: UIButton!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        doubleView = UIView()
+        doubleView.translatesAutoresizingMaskIntoConstraints = false
+
+        divider = UIView()
+        divider.translatesAutoresizingMaskIntoConstraints = false
         
-        // Add custom view sizing constraints here
+        leftButton = UIView()
+        leftButton.translatesAutoresizingMaskIntoConstraints = false
+
+        rightButton = UIView()
+        rightButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        nextKeyboardButton = UIButton(type: .system)
+        nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        nextKeyboardButton.setTitle("Next", for: .normal)
+
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
-        
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        loadGestureRecognizers()
+        loadSubviews()
+        addConstraints()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated
+    func loadGestureRecognizers() {
+        leftTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTapButton(sender:)))
+        leftButton.addGestureRecognizer(leftTapGestureRecognizer)
+        rightTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTapButton(sender:)))
+        rightButton.addGestureRecognizer(rightTapGestureRecognizer)
+    }
+    
+    func loadSubviews() {
+        view.addSubview(doubleView)
+        
+        divider.backgroundColor = .darkGray
+        doubleView.addSubview(divider)
+        
+        leftButton.backgroundColor = .white
+        doubleView.addSubview(leftButton)
+        
+        rightButton.backgroundColor = .white
+        doubleView.addSubview(rightButton)
+        
+        nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
+        nextKeyboardButton.sizeToFit()
+        doubleView.addSubview(nextKeyboardButton)
+    }
+    
+    func addConstraints() {
+        // doubleView
+        view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0))
+        
+        // divider
+        doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .centerX, relatedBy: .equal, toItem: doubleView, attribute: .centerX, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .top, relatedBy: .equal, toItem: doubleView, attribute: .top, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 2))
+        
+        // leftButton
+        doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .left, relatedBy: .equal, toItem: doubleView, attribute: .left, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .top, relatedBy: .equal, toItem: doubleView, attribute: .top, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .right, relatedBy: .equal, toItem: divider, attribute: .left, multiplier: 1, constant: 0))
+        
+        // rightButton
+        doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .left, relatedBy: .equal, toItem: divider, attribute: .right, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .top, relatedBy: .equal, toItem: doubleView, attribute: .top, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .right, relatedBy: .equal, toItem: doubleView, attribute: .right, multiplier: 1, constant: 0))
+        
+        // nextKeyboardButton
+        doubleView.addConstraint(NSLayoutConstraint(item: nextKeyboardButton, attribute: .left, relatedBy: .equal, toItem: doubleView, attribute: .left, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: nextKeyboardButton, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
+    }
+    
+    func didTapButton(sender: UITapGestureRecognizer) {
+        if (sender == leftTapGestureRecognizer) {
+            textDocumentProxy.insertText("A")
+            print("Left tapped")
+        } else if (sender == rightTapGestureRecognizer) {
+            textDocumentProxy.insertText("B")
+            print("Right tapped")
+        }
     }
     
     override func textWillChange(_ textInput: UITextInput?) {
@@ -47,15 +121,6 @@ class KeyboardViewController: UIInputViewController {
     
     override func textDidChange(_ textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
     }
 
 }
