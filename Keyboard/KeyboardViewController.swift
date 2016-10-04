f4963f7b6edb08a786edd4243bd8f4fc50ff5151
owen-yang <yoyoyango123@gmail.com>
2016-10-04 00:40:56 -0400

    Added upper/lower character input, added mode switch debug button

--- Keyboard/KeyboardViewController.swift
+++ Keyboard/KeyboardViewController.swift
@@ -10,34 +10,57 @@ import UIKit
 
 class KeyboardViewController: UIInputViewController {
     
-    var doubleView: UIView
-    var divider: UIView
-    var leftButton: UIView
-    var rightButton: UIView
+    let doubleView = UIView()
+    let divider = UIView()
     
-    var leftTapGestureRecognizer: UITapGestureRecognizer!
-    var rightTapGestureRecognizer: UITapGestureRecognizer!
+    let leftButton = UIView()
+    let leftLabel = UILabel()
+    let rightButton = UIView()
+    let rightLabel = UILabel()
     
-    var nextKeyboardButton: UIButton!
+    let leftTapGestureRecognizer = UITapGestureRecognizer()
+    let rightTapGestureRecognizer = UITapGestureRecognizer()
+    
+    let nextKeyboardButton = UIButton(type: .system)
+    let nextModeButton = UIButton(type: .system)
+    
+    let upperChars: [Character] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    let lowerChars: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    var currentCharSet: [Character]
+    
+    var leftLowerIndex = 0
+    var leftUpperIndex = 0
+    var rightLowerIndex = 0
+    var rightUpperIndex = 0
+    
+    enum Mode {
+        case upper
+        case lower
+        case numeral
+        case symbol
+    }
+    
+    var mode: Mode = .upper {
+        didSet {
+            resetButtonBounds()
+        }
+    }
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        doubleView = UIView()
-        doubleView.translatesAutoresizingMaskIntoConstraints = false
-
-        divider = UIView()
-        divider.translatesAutoresizingMaskIntoConstraints = false
+        leftLabel.font = leftLabel.font?.withSize(40)
+        rightLabel.font = rightLabel.font?.withSize(40)
         
-        leftButton = UIView()
-        leftButton.translatesAutoresizingMaskIntoConstraints = false
-
-        rightButton = UIView()
-        rightButton.translatesAutoresizingMaskIntoConstraints = false
+        nextKeyboardButton.setTitle("NextKB", for: .normal)
+        nextModeButton.setTitle("NextMode", for: .normal)
         
-        nextKeyboardButton = UIButton(type: .system)
-        nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        nextKeyboardButton.setTitle("Next", for: .normal)
+        currentCharSet = upperChars
 
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        resetButtonBounds()
+        nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
+        nextModeButton.addTarget(self, action: #selector(self.switchToNextMode), for: .touchUpInside)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -52,9 +75,9 @@ class KeyboardViewController: UIInputViewController {
     }
     
     func loadGestureRecognizers() {
-        leftTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTapButton(sender:)))
+        leftTapGestureRecognizer.addTarget(self, action: #selector(self.didTapButton(sender:)))
         leftButton.addGestureRecognizer(leftTapGestureRecognizer)
-        rightTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTapButton(sender:)))
+        rightTapGestureRecognizer.addTarget(self, action: #selector(self.didTapButton(sender:)))
         rightButton.addGestureRecognizer(rightTapGestureRecognizer)
     }
     
@@ -70,57 +93,147 @@ class KeyboardViewController: UIInputViewController {
         rightButton.backgroundColor = .white
         doubleView.addSubview(rightButton)
         
-        nextKeyboardButton.addTarget(self, action: #selector(UIInputViewController.advanceToNextInputMode), for: .touchUpInside)
-        nextKeyboardButton.sizeToFit()
+        leftButton.addSubview(leftLabel)
+        rightButton.addSubview(rightLabel)
+        
         doubleView.addSubview(nextKeyboardButton)
+        doubleView.addSubview(nextModeButton)
     }
     
     func addConstraints() {
         // doubleView
+        doubleView.translatesAutoresizingMaskIntoConstraints = false
         view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: doubleView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0))
         
         // divider
+        divider.translatesAutoresizingMaskIntoConstraints = false
         doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .centerX, relatedBy: .equal, toItem: doubleView, attribute: .centerX, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .top, relatedBy: .equal, toItem: doubleView, attribute: .top, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: divider, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 2))
         
         // leftButton
+        leftButton.translatesAutoresizingMaskIntoConstraints = false
         doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .left, relatedBy: .equal, toItem: doubleView, attribute: .left, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .top, relatedBy: .equal, toItem: doubleView, attribute: .top, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: leftButton, attribute: .right, relatedBy: .equal, toItem: divider, attribute: .left, multiplier: 1, constant: 0))
         
+        // leftLabel
+        leftLabel.translatesAutoresizingMaskIntoConstraints = false
+        leftButton.addConstraint(NSLayoutConstraint(item: leftLabel, attribute: .centerX, relatedBy: .equal, toItem: leftButton, attribute: .centerX, multiplier: 1, constant: 0))
+        leftButton.addConstraint(NSLayoutConstraint(item: leftLabel, attribute: .centerY, relatedBy: .equal, toItem: leftButton, attribute: .centerY, multiplier: 1, constant: 0))
+        
         // rightButton
+        rightButton.translatesAutoresizingMaskIntoConstraints = false
         doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .left, relatedBy: .equal, toItem: divider, attribute: .right, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .top, relatedBy: .equal, toItem: doubleView, attribute: .top, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: rightButton, attribute: .right, relatedBy: .equal, toItem: doubleView, attribute: .right, multiplier: 1, constant: 0))
         
+        // rightLabel
+        rightLabel.translatesAutoresizingMaskIntoConstraints = false
+        rightButton.addConstraint(NSLayoutConstraint(item: rightLabel, attribute: .centerX, relatedBy: .equal, toItem: rightButton, attribute: .centerX, multiplier: 1, constant: 0))
+        rightButton.addConstraint(NSLayoutConstraint(item: rightLabel, attribute: .centerY, relatedBy: .equal, toItem: rightButton, attribute: .centerY, multiplier: 1, constant: 0))
+        
         // nextKeyboardButton
+        nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
         doubleView.addConstraint(NSLayoutConstraint(item: nextKeyboardButton, attribute: .left, relatedBy: .equal, toItem: doubleView, attribute: .left, multiplier: 1, constant: 0))
         doubleView.addConstraint(NSLayoutConstraint(item: nextKeyboardButton, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
+        
+        // nextModeButton
+        nextModeButton.translatesAutoresizingMaskIntoConstraints = false
+        doubleView.addConstraint(NSLayoutConstraint(item: nextModeButton, attribute: .right, relatedBy: .equal, toItem: doubleView, attribute: .right, multiplier: 1, constant: 0))
+        doubleView.addConstraint(NSLayoutConstraint(item: nextModeButton, attribute: .bottom, relatedBy: .equal, toItem: doubleView, attribute: .bottom, multiplier: 1, constant: 0))
     }
     
     func didTapButton(sender: UITapGestureRecognizer) {
-        if (sender == leftTapGestureRecognizer) {
-            textDocumentProxy.insertText("A")
+        if sender == leftTapGestureRecognizer {
             print("Left tapped")
-        } else if (sender == rightTapGestureRecognizer) {
-            textDocumentProxy.insertText("B")
+        } else if sender == rightTapGestureRecognizer {
             print("Right tapped")
+        }
+        
+        updateButtonBounds(sender: sender)
+    }
+    
+    func updateButtonBounds(sender: UITapGestureRecognizer) {
+        if sender == leftTapGestureRecognizer {
+            if leftLowerIndex == leftUpperIndex {
+                textDocumentProxy.insertText("\(currentCharSet[leftLowerIndex])")
+                resetButtonBounds()
+            } else {
+                expandLeftBounds()
+            }
+        } else if sender == rightTapGestureRecognizer {
+            if rightLowerIndex == rightUpperIndex {
+                textDocumentProxy.insertText("\(currentCharSet[rightLowerIndex])")
+                resetButtonBounds()
+            } else {
+                expandRightBounds()
+            }
         }
     }
     
-    override func textWillChange(_ textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
+    func resetButtonBounds() {
+        leftLowerIndex = 0
+        leftUpperIndex = 12
+        rightLowerIndex = 13
+        rightUpperIndex = 25
+        updateButtonLabels()
+    }
+    
+    func expandLeftBounds() {
+        rightUpperIndex = leftUpperIndex
+        leftUpperIndex = (leftLowerIndex + leftUpperIndex) / 2
+        rightLowerIndex = leftUpperIndex + 1
+        updateButtonLabels()
+    }
+    
+    func expandRightBounds() {
+        leftLowerIndex = rightLowerIndex
+        leftUpperIndex = (rightLowerIndex + rightUpperIndex) / 2
+        rightLowerIndex = leftUpperIndex + 1
+        updateButtonLabels()
+    }
+    
+    func updateButtonLabels() {
+        switch mode {
+        case .upper:
+            fallthrough
+        case .lower:
+            if leftLowerIndex == leftUpperIndex {
+                leftLabel.text = "\(currentCharSet[leftLowerIndex])"
+            } else {
+                leftLabel.text = "\(currentCharSet[leftLowerIndex])...\(currentCharSet[leftUpperIndex])"
+            }
+            if rightLowerIndex == rightUpperIndex {
+                rightLabel.text = "\(currentCharSet[rightLowerIndex])"
+            } else {
+                rightLabel.text = "\(currentCharSet[rightLowerIndex])...\(currentCharSet[rightUpperIndex])"
+            }
+        default:
+            break
+        }
+    }
+    
+    func switchToNextMode() {
+        switch mode {
+        case .upper:
+            currentCharSet = lowerChars
+            mode = .lower
+        case .lower:
+            currentCharSet = upperChars
+            mode = .upper
+        default:
+            break
+        }
     }
     
     override func textDidChange(_ textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
+        resetButtonBounds()
     }
-
 }
